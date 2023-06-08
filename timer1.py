74cd20114c8964e30178c5cd2b261689db169c9a
skarb0624 <20221007@edu.hanbat.ac.kr>
2023-06-08 14:23:25 +0900

    완성

--- timer1.py
+++ timer1.py
@@ -17,6 +17,8 @@ GPIO.setmode(GPIO.BCM)
 global stop_message
 global intimer
 global push_button
+global input_time
+
 stop_message = 0
 push_button = 1
 intimer = 1
@@ -24,6 +26,7 @@ LED = 23
 BUTTON = 24
 buzzer_pin = 12
 save_time =1
+ledlight = True
 GPIO.setup(BUTTON, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(LED, GPIO.OUT)
 GPIO.setup(buzzer_pin, GPIO.OUT)
@@ -38,6 +41,8 @@ MQTT_SUB_TOPIC = "mobile/Namgyu/timer"
 
 def on_message(client,userdata, message) :
     global stop_message
+    global count
+    global input_time
     try:
         input_time = int(message.payload.decode("utf-8"))
         print(f"input times = {message.payload.decode('utf-8')}s")
@@ -45,22 +50,27 @@ def on_message(client,userdata, message) :
         countdown(input_time)
     except ValueError:
         print("Invalid input. Please provide a valid integer.")
+
+
 def countdown(timer):
     while timer > 0:
         global intimer
         minutes, seconds = divmod(timer, 60)
         timer_format = '{:02d}:{:02d}'.format(minutes, seconds)
-        print(timer_format, end='\r')  # 
-        time.sleep(1)  # 
+        print(timer_format, end='\r')
+        time.sleep(1)   
         timer -= 1
         if timer == 0 :
             print("timeout.")
             intimer = 0
+
 client =mqtt.Client()
 client.on_message = on_message
 client.connect(MQTT_HOST,MQTT_PORT,MQTT_KEEPALIVE_INTERVAL)
 client.subscribe(MQTT_SUB_TOPIC)
 client.loop_start()
+
+
 def message_timer() :
     if stop_message == 0 :
         timermean = {
@@ -70,19 +80,29 @@ def message_timer() :
         client.publish(MQTT_PUB_TOPIC,value)
         print(value)
         time.sleep(5)
+
+
+
 def message_button(channel) :
         global push_button
+        global ledlight
         if intimer == 0 :
             push_button = 0
             print("Stop using the timer.")
             p.stop()
+            ledlight = False
 GPIO.add_event_detect(BUTTON, GPIO.RISING, callback=message_button)
+
+
 def blink() :
-    for led in range (10) :
+    global ledlight
+    while ledlight :
         GPIO.output(23,GPIO.HIGH)
         time.sleep(0.5)
         GPIO.output(23,GPIO.LOW)
         time.sleep(0.3)
+
+
 try:
     while True :
         message_timer()
@@ -105,6 +125,7 @@ try:
 except KeyboardInterrupt:
     print("User terminated the program.")
     GPIO.output(23,GPIO.LOW)
+    GPIO.cleanup()
 finally:
     GPIO.output(23,GPIO.LOW)
     GPIO.cleanup()
